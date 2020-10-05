dfbb0f155a7a57da317155274410a51a4c7d89fb
Artem Ulko <ulkoart@gmail.com>
2020-10-05 12:41:39 +0300

    Initial Commit

---
+++ NewsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by user on 05/10/2020.
+//  Copyright © 2020 Artem Ulko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
